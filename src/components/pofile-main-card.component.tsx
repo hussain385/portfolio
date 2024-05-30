371b0785216b732ab2bc1e4028831c309f0502fd
hussain385 <56501305+hussain385@users.noreply.github.com>
2024-05-30 14:48:03 +0500

    Update pofile-main-card.component.tsx

--- src/components/pofile-main-card.component.tsx
+++ src/components/pofile-main-card.component.tsx
@@ -60,38 +60,6 @@ const ProfileMainCardComponent = () => {
                     </p>
                 </div>
             </div>
-            {/*<ProfileSectionComponent title="About Me">*/}
-            {/*    <p>I&apos;m a dedicated web and hybrid application developer with a solid track record...</p>*/}
-            {/*</ProfileSectionComponent>*/}
-            {/*<ProfileSectionComponent title="My Expertise">*/}
-            {/*    <ul className="list-disc list-inside">*/}
-            {/*        <li>Specialize in building responsive and high-performance web applications...</li>*/}
-            {/*        <li>Excel in hybrid app development using React Native and Flutter...</li>*/}
-            {/*    </ul>*/}
-            {/*</ProfileSectionComponent>*/}
-            {/*<ProfileSectionComponent title="Problem-Solving">*/}
-            {/*    <p>I thrive on solving complex challenges...</p>*/}
-            {/*</ProfileSectionComponent>*/}
-            {/*<ProfileSectionComponent title="Tech Stack">*/}
-            {/*    <p>Front-End: HTML5, CSS3, JavaScript, React, Vue.js</p>*/}
-            {/*    <p>Mobile App: React Native, Flutter</p>*/}
-            {/*    <p>Back-End: Node.js, Express.js, MongoDB</p>*/}
-            {/*    <p>Tools: Git, VS Code, JIRA</p>*/}
-            {/*</ProfileSectionComponent>*/}
-            {/*<ProfileSectionComponent title="Open Source Advocate">*/}
-            {/*    <p>I&apos;m a strong advocate for the open-source community...</p>*/}
-            {/*</ProfileSectionComponent>*/}
-            {/*<ProfileSectionComponent title="Lifelong Learning">*/}
-            {/*    <p>In the fast-paced tech landscape, continuous learning is key...</p>*/}
-            {/*</ProfileSectionComponent>*/}
-            {/*<ProfileSectionComponent title="Let's Connect">*/}
-            {/*    <p>I&apos;m always eager to connect with fellow developers, tech enthusiasts...</p>*/}
-            {/*</ProfileSectionComponent>*/}
-            {/*<ProfileSectionComponent title="Contact Me">*/}
-            {/*    <p>Email: hussainquetta53@gmail.com</p>*/}
-            {/*    <p>LinkedIn: <a href="https://www.linkedin.com/in/hussain-saifuddin786" className="text-blue-400">linkedin.com/in/hussain-saifuddin786</a></p>*/}
-            {/*    <p>Portfolio: <a href="https://hussain-saifuddin.surge.sh" className="text-blue-400">hussain-saifuddin.surge.sh</a></p>*/}
-            {/*</ProfileSectionComponent>*/}
         </div>
     );
 };
